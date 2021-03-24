f3df7d098b80bf7b4939b5c5369806f7c3ed70a3
NicolaiOkkels <nico902t@stud.kea.dk>
2021-03-24 23:38:03 +0100

    added commented code to show method to read from file

--- src/app/ReadFile.java
+++ src/app/ReadFile.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -20,6 +21,19 @@ public class ReadFile {
         try {
             //note: Lambda expressions: parameter -> expression or parameter -> { code block } for more complex code
             //note: Lambda expressions are similar to method, but do not need a name and can be used directly in method body
+
+            /*This commented code does the same as the lambda expression below and reads from a txt file
+            File file = new File("Grupper.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String s = scanner.nextLine();
+                groups.add(s.split(";")[0]);
+                mails.add(s.split(";")[1]);
+                names.add(s.split(";")[2]);
+                lastnames.add(s.split(";")[3]);
+                lineCount.getAndIncrement();
+                }
+            */
             Stream<String> stream = Files.lines(Paths.get("Grupper.txt")); //reads all lines from a file as a stream
             stream.forEach(s -> {
                 //When the string is split at ; it is split into a array where [number] == string
@@ -31,7 +45,7 @@ public class ReadFile {
             });
             //adds all students list
             for (int i = 0; i < lineCount.intValue(); i++) {
-                Student student = new Student(groups.get(i),mails.get(i),names.get(i),lastnames.get(i));
+                Student student = new Student(groups.get(i), mails.get(i), names.get(i), lastnames.get(i));
                 students.add(student);
             }
         } catch (IOException ioException) {
@@ -48,7 +62,7 @@ public class ReadFile {
             Stream<String> stream = Files.lines(Paths.get("Grupper.txt")); //reads all lines from a file as a stream
             stream.forEach(s -> {
                 //When the string is split at ; it is split into a array where [number] == string
-                Student student= new Student(s.split(";")[0],s.split(";")[1],s.split(";")[2],s.split(";")[3]);
+                Student student = new Student(s.split(";")[0], s.split(";")[1], s.split(";")[2], s.split(";")[3]);
                 students.add(student);
             });
         } catch (IOException ioException) {
